b3b37b80ef5c9c874a6f4572a17f0e63e10f6bc9
Gustavo Almeida <gupalmeida@gmail.com>
2025-10-24 23:37:26 +0200

    Adding simple implementation of queue data structure using python list and O(n) time + O(1) space complexities for dequeueing method.

---
+++ DSA/queue/queue_with_python_list.py
@@ -0,0 +1,51 @@
+class Queue:
+
+    def __init__(self):
+        self.items = []
+
+    def __str__(self):
+        return " ".join([str(e) for e in self.items])
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def queue(self, value):
+        self.items.append(value)
+
+    def dequeue(self):
+        """ As queue is a FIFO data structure
+        the first element must be the one removed
+        and than the remaining should have their
+        positions shifted.
+        To achieve that, the pop method is the
+        selected method given it will provide a
+        O(n) time + O(1) space.
+        A better method like popleft could be
+        implemented, as it would provide O(1)
+        in both space and time, but let's keep
+        this way for now."""
+        if self.is_empty():
+            return "Queue is empty"
+        else:
+            return self.items.pop(0)
+
+    def peek(self):
+        if self.is_empty():
+            return "Queue is empty"
+        else:
+            return self.items[0]
+        
+
+if __name__ == "__main__":
+    q = Queue()
+    q.queue(10)
+    q.queue(20)
+    q.queue(30)
+    q.queue(40)
+    print(q)
+    dequeued_elements = []
+    for _ in range(2):
+        dequeued_elements.append( q.dequeue() )
+    print(f'Dequeued elements: {" ".join([str(e) for e in dequeued_elements])}')
+    print("After dequeueing")
+    print(q)
